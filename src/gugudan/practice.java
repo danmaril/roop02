20e27be278fcb888ee860111328732ca24165165
JBEDU18 <JBEDU18@DESKTOP-AELTP4L>
2022-03-22 12:05:01 +0900

    구구단 연습

---
+++ src/gugudan/practice.java
@@ -0,0 +1,15 @@
+package gugudan;
+
+import java.util.*;
+
+public class practice {
+	public static void main(String[] args) {
+		for(int i=1; i<10; i++);
+			for(int j=1; j<10; j++) {
+				String i;
+				System.out.print(i + "*" + j +"=" + i*j);
+				System.out.print('\t');
+			}
+			System.out.println();
+	}
+}
